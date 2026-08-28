f776eee62188f0ecb9f0413393825cfdd03d2397
Chris <sorensencc@gmail.com>
2026-08-28 12:23:32 -0400

    feat(trm-devops): implement queue reconciler and atomic markdown generator

--- modules/trm-devops/src/index.ts
+++ modules/trm-devops/src/index.ts
@@ -3,3 +3,4 @@ export * from "./core/normalizer.ts";
 export * from "./core/lock.ts";
 export * from "./core/extractor.ts";
 export * from "./core/notebooklm-client.ts";
+export * from "./core/reconciler.ts";
