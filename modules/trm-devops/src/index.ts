4dce7c897a96e3576dc8eb5b25d833b4d0d8c863
Chris <sorensencc@gmail.com>
2026-08-28 12:08:17 -0400

    feat(trm-devops): implement offline fallback buffer and notebooklm client bridge

--- modules/trm-devops/src/index.ts
+++ modules/trm-devops/src/index.ts
@@ -2,3 +2,4 @@ export * from "./core/types.ts";
 export * from "./core/normalizer.ts";
 export * from "./core/lock.ts";
 export * from "./core/extractor.ts";
+export * from "./core/notebooklm-client.ts";
